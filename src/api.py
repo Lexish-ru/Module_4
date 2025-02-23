bfb9b67d60a629bf90f8ded595d0d0f986bf8021
Aleksey Stolbov <alstofly@gmail.com>
2025-02-23 18:45:27 +0300

    КР_2_добавлен форматированый вывод вакансий

--- src/api.py
+++ src/api.py
@@ -1,8 +1,7 @@
-from http.client import responses
-
 import requests
 from abc import ABC, abstractmethod
 from typing import List, Dict, Any, Optional
+from vacancy import Vacancy
 
 
 class BaseAPI(ABC):
@@ -13,13 +12,13 @@ class BaseAPI(ABC):
         """Метод получения вакансий. Должен быть реализован в наследниках"""
         pass
 
-class HeadHunteAPI(BaseAPI):
+class HeadHunterAPI(BaseAPI):
     """Класс для работы с API HH.ru"""
 
     BASE_URL: str = "https://api.hh.ru/vacancies"
 
     def get_vacancies(self, keyword: str, area: int = 1, per_page: int = 10)-> List[Dict[str, Any]]:
-        """получает вакансии по ключевому слову"""
+        """Получает вакансии по ключевому слову"""
         params: Dict[str, Any] = {
             "text": keyword,
             "area": area,
@@ -32,23 +31,27 @@ class HeadHunteAPI(BaseAPI):
             print(f"Ошибка: {response.status_code}")
             return []
 
-        data: Dict[str,Any] = response.json()
-        vacancies: List[Dict[str, Any]] = []
+        data: Dict[str, Any] = response.json()
+        vacancies: List[Vacancy] = []
 
         for item in data.get("items", []):
-            vacancies.append({
-                "name": item.get("name", "Без названия"),
-                "url": item.get("alternate_url", "Нет ссылки"),
-                "salary": item.get("salary"),
-                "employer": item.get("employer", {}).get("name", "Не указан"),
-                "requirement": item.get("snippet", {}).get("requirement", "Не указано"),
-                "responsibility": item.get("snippet", {}).get("responsibility", "Не указано")
-            })
+            salary_info = item.get("salary", {})
+            vacancy = Vacancy(
+                name=item.get("name", "Без названия"),
+                url=item.get("alternate_url", "Нет ссылки"),
+                salary_from=salary_info.get("from"),
+                salary_to=salary_info.get("to"),
+                currency=salary_info.get("currency"),
+                employer=item.get("employer", {}).get("name", "Не указан"),
+                requirement=item.get("snippet", {}).get("requirement", "Не указано"),
+                responsibility=item.get("snippet", {}).get("responsibility", "Не указано")
+            )
+            vacancies.append(vacancy)
 
         return vacancies
 
 if __name__ == "__main__":
-    hh = HeadHunteAPI()
+    hh = HeadHunterAPI()
     vacancies = hh.get_vacancies("Python разработчик", area=2, per_page=5)
 
     for v in vacancies:
